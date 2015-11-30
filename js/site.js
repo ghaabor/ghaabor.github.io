4021241e9de00d06ba5232601eec1636601691d5
Gabor Takacs <me@gabortakacs.com>
2015-11-30 17:54:53 +0100

    Email link fixed

--- js/site.js
+++ js/site.js
@@ -11,10 +11,26 @@ $(function(){
           $(".social-link").each(function(){
             $(this).delay(timeOffset).css("visibility", "visible").animate(
               {opacity: 1.0}, 500
-            );
+              );
             timeOffset += 750;
           });
         }
       });
-  });
+    });
+  $("#mail").click(
+    function() {
+      var username, domain, subject, mailto;
+
+      // Decode email address
+      username = $(this).attr("data-username").split("").reverse().join("")
+      domain = $(this).attr("data-domain").split("").reverse().join("")
+      mailto = username + "@" + domain
+      // Add subject
+      subject = $(this).attr("data-subject")
+      if (subject.length > 0) {
+        mailto += "?subject="+subject
+      }
+
+      window.location.href = "mailto:" + mailto;
+    })
 });
