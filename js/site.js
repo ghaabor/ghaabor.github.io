774d66a1a0c50ac445e476995c11e0f2905372d5
Gabor Takacs <me@gabortakacs.com>
2015-11-30 01:03:45 +0100

    Extra animation

--- js/site.js
+++ js/site.js
@@ -1,8 +1,20 @@
 $(function(){
-  $("span.name").typed({
-    strings: ["g^200ab^150or_^100taka^250cs"],
-    typeSpeed: 150,
-    startDelay: 1000,
-    cursorChar: "_"
+  $("p#name").css("visibility", "visible").animate(
+    {opacity: 1.0}, 2000, function(){
+      $("span.name").typed({
+        strings: ["g^200ab^150or_^100taka^250cs"],
+        typeSpeed: 150,
+        startDelay: 1000,
+        cursorChar: "_", 
+        callback: function(){
+          var timeOffset = 500;
+          $(".social-link").each(function(){
+            $(this).delay(timeOffset).css("visibility", "visible").animate(
+              {opacity: 1.0}, 500
+            );
+            timeOffset += 750;
+          });
+        }
+      });
   });
 });
